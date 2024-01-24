158544891441f3d278c382fc6a95e5501894171f
Yassperx <elmessariyassine15@gmail.com>
2024-01-24 19:00:45 +0100

    nice

--- model.py
+++ model.py
@@ -1,99 +1,48 @@
-import numpy as np
+# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
+# License: BSD 3 clause
+
+# Standard scientific Python imports
 import matplotlib.pyplot as plt
-from sklearn.metrics import accuracy_score, log_loss
-from tqdm import tqdm
 
-def initialisation(dimensions):
-    parametres = {}
-    C = len(dimensions)
+# Import datasets, classifiers and performance metrics
+from sklearn import datasets, metrics, svm
+from sklearn.model_selection import train_test_split
 
-    np.random.seed(1)
+digits = datasets.load_digits()
 
-    for c in range(1, C):
-        parametres['W' + str(c)] = np.random.randn(dimensions[c], dimensions[c - 1])
-        parametres['b' + str(c)] = np.random.randn(dimensions[c], 1)
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, label in zip(axes, digits.images, digits.target):
+    ax.set_axis_off()
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    ax.set_title("Training: %i" % label)
 
-    return parametres
+# flatten the images
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
 
-def forward_propagation(X, parametres):
-  
-  activations = {'A0': X}
+# Create a classifier: a support vector classifier
+clf = svm.SVC(gamma=0.001)
 
-  C = len(parametres) // 2
+# Split data into 50% train and 50% test subsets
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False
+)
 
-  for c in range(1, C + 1):
+# Learn the digits on the train subset
+clf.fit(X_train, y_train)
 
-    Z = parametres['W' + str(c)].dot(activations['A' + str(c - 1)]) + parametres['b' + str(c)]
-    activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
+# Predict the value of the digit on the test subset
+predicted = clf.predict(X_test)
 
-  return activations
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, prediction in zip(axes, X_test, predicted):
+    ax.set_axis_off()
+    image = image.reshape(8, 8)
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    ax.set_title(f"Prediction: {prediction}")
 
-def back_propagation(y, parametres, activations):
+disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+disp.figure_.suptitle("Confusion Matrix")
+print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
-  m = y.shape[1]
-  C = len(parametres) // 2
-
-  dZ = activations['A' + str(C)] - y
-  gradients = {}
-
-  for c in reversed(range(1, C + 1)):
-    gradients['dW' + str(c)] = 1/m * np.dot(dZ, activations['A' + str(c - 1)].T)
-    gradients['db' + str(c)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
-    if c > 1:
-      dZ = np.dot(parametres['W' + str(c)].T, dZ) * activations['A' + str(c - 1)] * (1 - activations['A' + str(c - 1)])
-
-  return gradients
-
-def update(gradients, parametres, learning_rate):
-
-    C = len(parametres) // 2
-
-    for c in range(1, C + 1):
-        parametres['W' + str(c)] = parametres['W' + str(c)] - learning_rate * gradients['dW' + str(c)]
-        parametres['b' + str(c)] = parametres['b' + str(c)] - learning_rate * gradients['db' + str(c)]
-
-    return parametres
-
-def predict(X, parametres):
-  activations = forward_propagation(X, parametres)
-  C = len(parametres) // 2
-  Af = activations['A' + str(C)]
-  return Af >= 0.5
-
-def deep_neural_network(X, y, hidden_layers = (16, 16, 16), learning_rate = 0.001, n_iter = 3000):
-    # initialisation parametres
-    dimensions = list(hidden_layers)
-    dimensions.insert(0, X.shape[0])
-    dimensions.append(y.shape[0])
-    np.random.seed(1)
-    parametres = initialisation(dimensions)
-
-    # tableau numpy contenant les futures accuracy et log_loss
-    training_history = np.zeros((int(n_iter), 2))
-
-    C = len(parametres) // 2
-
-    # gradient descent
-    for i in tqdm(range(n_iter)):
-
-        activations = forward_propagation(X, parametres)
-        gradients = back_propagation(y, parametres, activations)
-        parametres = update(gradients, parametres, learning_rate)
-        Af = activations['A' + str(C)]
-
-        # calcul du log_loss et de l'accuracy
-        training_history[i, 0] = (log_loss(y.flatten(), Af.flatten()))
-        y_pred = predict(X, parametres)
-        training_history[i, 1] = (accuracy_score(y.flatten(), y_pred.flatten()))
-
-    # Plot courbe d'apprentissage
-    plt.figure(figsize=(12, 4))
-    plt.subplot(1, 2, 1)
-    plt.plot(training_history[:, 0], label='train loss')
-    plt.legend()
-    plt.subplot(1, 2, 2)
-    plt.plot(training_history[:, 1], label='train acc')
-    plt.legend()
-    plt.show()
-
-    return training_history
+plt.show()
